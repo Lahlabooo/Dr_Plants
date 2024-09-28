aaa99fa1952e590678c2f9974005d4b1f661f9f7
Lahlabooo <ellahloby22@gmail.com>
2024-09-28 04:20:06 +0300

    add DialogeResult to Scan Page

--- lib/Features/Scan/Presentation/Views/Widgets/AlertDialoge_Body.dart
+++ lib/Features/Scan/Presentation/Views/Widgets/AlertDialoge_Body.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:planta/Core/Utils/Styles.dart';
 import 'package:planta/Features/Scan/Presentation/Manger/Get_Plant_State_Info/get_plant_state_info_cubit.dart';
 import '../../../../../Core/Widgets/CustomErrorWidget.dart';
 import '../../../../../Core/Widgets/Custom_loading_Indicator.dart';
-import 'Dialoge_Scan_Result_Item.dart';
+import 'Dialoge_Result.dart';
 
 class AlertDialogeBody extends StatelessWidget {
   const AlertDialogeBody({
@@ -13,62 +12,27 @@ class AlertDialogeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GetPlantStateInfoCubit,GetPlantStateInfoState>(
+    return BlocBuilder<GetPlantStateInfoCubit, GetPlantStateInfoState>(
       builder: (BuildContext context, state) {
-        if (state is GetPlantStateInfoFailure){
-          return  Padding(
+        if (state is GetPlantStateInfoFailure) {
+          return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0),
             child: SizedBox(
-              height: MediaQuery.of(context).size.height*.3,
-              child:CustomErrorWidget(title:state.errorMessage,)
-            ),
+                height: MediaQuery.of(context).size.height * .3,
+                child: CustomErrorWidget(
+                  title: state.errorMessage,
+                )),
           );
-        }
-        else if (state is GetPlantStateInfoSuccess){
-          return Column(
-            children: [
-              const SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Result',
-                style: Styles.textStyle25.copyWith(color: Colors.black),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  DialogeScanResultItem(
-                    txt: state.plantModel.plant ?? "no_Value",
-                    title: 'Plant',
-                  ),
-                  DialogeScanResultItem(
-                    txt: state.plantModel.status ?? "no_Value",
-                    title: 'States',
-                  ),
-                  DialogeScanResultItem(
-                    txt: state.plantModel.disease ??"no_Value",
-                    title: 'Disease',
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-            ],
+        } else if (state is GetPlantStateInfoSuccess) {
+          return DialogeReult(
+            plantModel: state.plantModel,
           );
-        }
-        else {
+        } else {
           return SizedBox(
-              height: MediaQuery.of(context).size.height*.3,
+              height: MediaQuery.of(context).size.height * .3,
               child: const CustomLoadingIndicator());
         }
-
       },
-
     );
   }
 }
-
---
+++ lib/Features/Scan/Presentation/Views/Widgets/Dialoge_Result.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:planta/Core/Utils/Styles.dart';
+import 'package:planta/Features/Scan/Data/Models/Plant_State_Info.dart';
+
+import 'Dialoge_Scan_Result_Item.dart';
+
+class DialogeReult extends StatelessWidget {
+  const DialogeReult({
+    super.key,
+    required this.plantModel,
+  });
+  final PlantStateInfoModel plantModel;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          'Result',
+          style: Styles.textStyle25.copyWith(color: Colors.black),
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            DialogeScanResultItem(
+              txt: plantModel.plant ?? "no_Value",
+              title: 'Plant',
+            ),
+            DialogeScanResultItem(
+              txt: plantModel.status ?? "no_Value",
+              title: 'States',
+            ),
+            DialogeScanResultItem(
+              txt: plantModel.disease ?? "no_Value",
+              title: 'Disease',
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+      ],
+    );
+  }
+}
